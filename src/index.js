62a886736785a8cee76cc0330751ea836df5ff12
Kunal31git <mekunal31@gmail.com>
2024-08-26 23:02:09 +0530

    Utils config

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants.js";
 import connectDB from "./db/dbConnect.js" ; 
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Failed to connect to MongoDB",error);
+})
 
 
 
